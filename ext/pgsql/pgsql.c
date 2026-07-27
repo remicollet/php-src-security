a33d0c050d938889930c6a6a403d0a563c968301
Ilija Tovilo <ilija.tovilo@me.com>
2026-07-27 16:49:38 +0200

    Fix SQL injection in ext-pgsql via E'...' backslash breakout
    php_pgsql_add_quotes() quotes the string with E'...', but PQescapeStringConn()
    does not escape \ unless standard_conforming_strings is off.
    PQescapeStringConn() is meant to be used with '', so do that instead.
    
    Fixes GHSA-7qpv-r5mr-78m4
    
    (cherry picked from commit ab048bd83b578119cf81b456526d50498421d617)
    (cherry picked from commit eb0de306e0569f8b50065a42f13dd3e2b3729532)
    (cherry picked from commit 531196af8ccc43c26ca97aef27b0816e8b4e2f60)
    (cherry picked from commit 6a80d9d36a5ea210ea6cc411864c08c71f7417ec)
    (cherry picked from commit 72c3531150e451cdfca49b16879257c6de6e2880)

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -5813,7 +5813,6 @@ static int php_pgsql_add_quotes(zval *src, zend_bool should_free)
 	assert(Z_TYPE_P(src) == IS_STRING);
 	assert(should_free == 1 || should_free == 0);
 
-	smart_str_appendc(&str, 'E');
 	smart_str_appendc(&str, '\'');
 	smart_str_appendl(&str, Z_STRVAL_P(src), Z_STRLEN_P(src));
 	smart_str_appendc(&str, '\'');
@@ -6093,7 +6092,6 @@ PHP_PGSQL_API int php_pgsql_convert(PGconn *pg_link, const char *table_name, con
 							zend_string *str;
 							/* PostgreSQL ignores \0 */
 							str = zend_string_alloc(Z_STRLEN_P(val) * 2, 0);
-							/* better to use PGSQLescapeLiteral since PGescapeStringConn does not handle special \ */
 							ZSTR_LEN(str) = PQescapeStringConn(pg_link, ZSTR_VAL(str), Z_STRVAL_P(val), Z_STRLEN_P(val), NULL);
 							str = zend_string_truncate(str, ZSTR_LEN(str), 0);
 							ZVAL_NEW_STR(&new_val, str);
